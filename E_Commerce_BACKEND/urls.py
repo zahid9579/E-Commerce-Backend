6f6bb7f7bdc6a5e9777f5ed3181dd29f99549698
Zahid-9579 <zahidansari.er@gmail.com>
2025-05-17 23:07:12 +0530

    JWT authentication done for user authentication

--- E_Commerce_BACKEND/urls.py
+++ E_Commerce_BACKEND/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     
-    
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
